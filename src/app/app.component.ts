7b7f28e07889814da6cdaf02ade37cf529bffdf7
msiddiqi <muhammadsiddiqi@Muhammads-MacBook-Pro.local>
2018-08-29 22:12:24 -0400

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MatIconRegistry} from '@angular/material';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'button-toggle-app';
+
+  toggleOptions: Array<String> = ["First", "Second"];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatButtonToggleModule, MatIconModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonToggleModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule    
   ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent]
 })
